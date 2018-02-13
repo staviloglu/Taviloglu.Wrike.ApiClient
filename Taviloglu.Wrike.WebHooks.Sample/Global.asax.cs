a2619d4f52fb0b7dd4f5f668cd0c168e8b05435f
Sinan Taviloglu <staviloglu@gmail.com>
2018-02-13 17:13:35 +0300

    triyng to implement webhooks

--- Taviloglu.Wrike.WebHooks.Sample/Global.asax.cs
+++ Taviloglu.Wrike.WebHooks.Sample/Global.asax.cs
@@ -2,11 +2,13 @@
 
 namespace Taviloglu.Wrike.WebHooks.Sample
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class Global : System.Web.HttpApplication
     {
+
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
+
     }
 }
