4f06ce128c7689939cfe3a124cf88f91ae069307
Sinan Taviloglu <staviloglu@gmail.com>
2018-03-01 01:05:39 +0300

    - AttachmentsExtensions added - TasksExtensions added

---
+++ Taviloglu.Wrike.ApiClient.Extensions/AttachmentsExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taviloglu.Wrike.ApiClient.Extensions
+{
+    public static class AttachmentsExtension
+    {
+        public static async Task DownloadAndSaveAttachment(this IWrikeAttachmentsClient wrikeAttachmentsClient, string attachmentId, string savingPath)
+        {
+            using (var downloadedStream = await wrikeAttachmentsClient.DownloadAsync(attachmentId))
+            {
+                using (FileStream fs = new FileStream(savingPath, FileMode.CreateNew, FileAccess.Write))
+                {
+                    byte[] buffer = new byte[8 * 1024];
+                    int len;
+                    while ((len = downloadedStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        fs.Write(buffer, 0, len);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Taviloglu.Wrike.ApiClient.Extensions/TasksExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taviloglu.Wrike.Core;
+
+namespace Taviloglu.Wrike.ApiClient.Extensions
+{
+    public static class TasksExtensions
+    {
+        public static async Task<WrikeTask> GetTaskByIdAsync(this IWrikeTasksClient wrikeTasksClient, string taskId)
+        {
+            if (string.IsNullOrWhiteSpace(taskId))
+            {
+                throw new ArgumentNullException(nameof(taskId));
+            }
+
+            var tasks = await wrikeTasksClient.GetAsync(taskIds: new List<string> { taskId});
+            return tasks.FirstOrDefault();
+        }
+
+
+        public static async Task<List<WrikeTask>> GetSubTasksBySuperTaskIdAsync(this IWrikeTasksClient wrikeTasksClient, string superTaskId)
+        {
+            if (string.IsNullOrWhiteSpace(superTaskId))
+            {
+                throw new ArgumentNullException(nameof(superTaskId));
+            }
+
+            var superTask = await GetTaskByIdAsync(wrikeTasksClient, superTaskId);
+
+            if (superTask == null)
+            {
+                return new List<WrikeTask>();
+            }
+
+            return await wrikeTasksClient.GetAsync(taskIds: superTask.SubTaskIds);            
+        }
+    }
+}
