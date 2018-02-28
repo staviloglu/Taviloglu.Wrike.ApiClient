023ffefa8a52c9df4b51fa81663763c813c0f663
Sinan Taviloglu <staviloglu@gmail.com>
2018-03-01 01:06:04 +0300

    - special case for BookAndAd

--- Taviloglu.Wrike.ApiClient.Samples/BookAndAdWorkflow.cs
+++ Taviloglu.Wrike.ApiClient.Samples/BookAndAdWorkflow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Taviloglu.Wrike.ApiClient.Extensions;
@@ -9,6 +10,8 @@ namespace Taviloglu.Wrike.ApiClient.Samples
 {
     public static class BookAndAdWorkflow
     {
+        private static string MiraculousTaskId;
+        private const string  MiraculousDirectoryPath = @"C:\Users\Sinan\Desktop\downloadedAttachments";
         public static async Task Run(WrikeClient client)
         {
             //TODO: önceden buId yi kaydetmiş de olabiliriz, email ile bulabiliriz de...
@@ -22,9 +25,25 @@ namespace Taviloglu.Wrike.ApiClient.Samples
             await CreateCampaignTask(client, rootFolderId, folderId, trashCanUserId);
 
             //TODO: trashcan gerekli operasyonları yapıp, taskın statusu güncellediğinde bize taskId gelecek ve bu çalıştırılacak
-            await AssignMostAwailableUserToTask(client, folderId, trashCanUserId, "taskId");
+            await AssignMostAwailableUserToTask(client, folderId, trashCanUserId, MiraculousTaskId);
 
             //TODO: task tamamlanıp statusu güncellenince bize taskId gelecek onunla Attachmentlarını download edeceğiz.
+            await DownloadAttachmentsOfTheTask(client, MiraculousTaskId);
+        }
+
+        public async static Task DownloadAttachmentsOfTheTask(WrikeClient client, string mainTaskId)
+        {
+            var subTasks = await client.Tasks.GetSubTasksBySuperTaskIdAsync(mainTaskId);
+            foreach (var subTask in subTasks)
+            {
+                var attachments = await client.Attachments.GetAsync(taskId: subTask.Id, withUrls: true);
+                foreach (var attachment in attachments)
+                {
+                    var savePath = Path.Combine(MiraculousDirectoryPath, attachment.Name);
+                    await client.Attachments.DownloadAndSaveAttachment(attachment.Id, savePath);
+                }
+            }
+
         }
 
         public static WrikeTask GetSubTask(string title, string description, string superTaskId, string trashCanUserId)
@@ -40,7 +59,7 @@ namespace Taviloglu.Wrike.ApiClient.Samples
 
         public async static Task<string> GetTrashCanUserId(WrikeClient client, string email)
         {
-            var trashCanUser = await client.Contacts.GetByEmailAsync(email);
+            var trashCanUser = await client.Contacts.GetContactByEmailAsync(email);
 
             if (trashCanUser == null)
             {
@@ -57,18 +76,22 @@ namespace Taviloglu.Wrike.ApiClient.Samples
             string offerName,
             string offerDueDates,
             string contacts,
-            string offerNotes)
+            string offerNotes,
+            string offerId)
         {
             var description =
                 $"Company: {companyRealName}<br/>Assoc.: {associateName}<br/>Assoc. Email: {associateEmail}<br/>Assoc. Phone: {associatePhoneNumber}<br/>Contacts: {contacts}<br/>Billing Address: {billingAddress}<br/>Notes: {offerNotes}";
 
             //TODO: duedates değerlerini de kullanalalım mı task için set edebiliriz?
 
+            string offerIdCustomFieldId = "IEABX2HEJUAAUBEA";
+
             return new WrikeTask
             {
                 Title = offerName,
                 Description = description,
-                ResponsibleIds = new List<string> { responsibleUserId }
+                ResponsibleIds = new List<string> { responsibleUserId },
+                CustomFields = new List<WrikeCustomFieldData> { new WrikeCustomFieldData(offerIdCustomFieldId, offerId) }
             };
         }
 
@@ -82,10 +105,14 @@ namespace Taviloglu.Wrike.ApiClient.Samples
                 "Offer Name #1",
                 "26.02.2018 - 02.04.2018",
                 "Contacts...",
-                "Special notes about offer");
+                "Special notes about offer",
+                "12854");
+
 
             //mainTaskı yaratırken içinde bulunacağı folderı veriyoruz
             superTask = await client.Tasks.CreateAsync(folderId, superTask);
+
+            MiraculousTaskId = superTask.Id;
 
             //her productiondan bir subTask yarat
             var subTasks = new List<WrikeTask>
@@ -102,27 +129,34 @@ namespace Taviloglu.Wrike.ApiClient.Samples
             }
         }
 
-        public static string GetUserIdWithMinimumTaskCount(List<WrikeTask> taskList, List<string> userList)
+        public static string GetUserIdWithMinimumWorkload(List<WrikeTask> taskList, List<string> userList, bool countSubTasks)
         {
             //TODO: her user için bütün taskları tekrar geziyor, bunun yerine taskları gezip userlara count atama yapmak daha iyi olabilir.
-            int taskCount = 0;
-            int minTaskCount = int.MaxValue;
+            int usersWorkload = 0;
+            int minWorkload = int.MaxValue;
             string userIdToAssign = string.Empty;
             foreach (var userId in userList)
             {
-                taskCount = 0;
+                usersWorkload = 0;
                 foreach (var task in taskList)
                 {
                     if (task.ResponsibleIds.Count < 1
                         || !task.ResponsibleIds.Any(id => id.Equals(userId))) continue;
 
-                    taskCount++;
+                    if (countSubTasks)
+                    {
+                        usersWorkload += task.SubTaskIds.Count;
+                    }
+                    else
+                    {
+                        usersWorkload++;
+                    }                    
                 }
 
-                if (taskCount < minTaskCount)
+                if (usersWorkload < minWorkload)
                 {
                     userIdToAssign = userId;
-                    minTaskCount = taskCount;
+                    minWorkload = usersWorkload;
                 }
             }
 
@@ -137,19 +171,20 @@ namespace Taviloglu.Wrike.ApiClient.Samples
 
             if (taskList.Count>0)
             {
-                taskId = taskList[0].Id;
                 //TODO:
                 //task ataması yapılabilecek UserId listesi önceden girilmiş olabilir
                 //wriketaki userlar çekilip trashcan olmayanların tamamı diye kabul edilebilir
 
                 //TODO: accounta bağlı ne kadar person tipinde user varsa al, bu kişiler özel bir account ile de ayrılabilir.
                 //ya da id leri önceden biliniyor olabilir.
-                var userList = await client.Contacts.GetByTypeAsync(WrikeUserType.Person);
+                var userList = await client.Contacts.GetContactsByTypeAsync(WrikeUserType.Person);
                 var userIds = userList.Select(u => u.Id).ToList();
 
-                var userId = GetUserIdWithMinimumTaskCount(taskList, userIds); //mot available user to assign the task
+                var userId = GetUserIdWithMinimumWorkload(taskList, userIds, true); //most available user to assign the task
 
                 var customStatusId = "IEABX2HEJMAIOJKE"; //customStatusId to move the task (Assigned List)
+
+                
 
                 //ilgili taskı trashCan rolunden al bulduğun usera assign et ve ilgili customStatuse çek
                 var updatedTask = await client.Tasks.UpdateAsync(
