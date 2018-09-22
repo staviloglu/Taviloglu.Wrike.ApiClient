fc49cc75c6d0a588df0b40f7d5479b781002ecfa
Sinan Taviloglu <staviloglu@gmail.com>
2018-09-22 20:24:43 +0300

    - Added constants to reduce test function count

--- Taviloglu.Wrike.ApiClient.Tests.Unit/Contacts/ContactsTests.cs
+++ Taviloglu.Wrike.ApiClient.Tests.Unit/Contacts/ContactsTests.cs
@@ -7,64 +7,30 @@ namespace Taviloglu.Wrike.ApiClient.Tests.Unit.Contacts
     [TestFixture]
     public class ContactsTests
     {
-        WrikeClient _wrikeClient;
-
-        [OneTimeSetUp]
-        public void Setup()
-        {
-            _wrikeClient = new WrikeClient("eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjIzMTc2ODQsXCJpXCI6NTM3NDAyNCxcImNcIjo0NTk1MDE0LFwidlwiOm51bGwsXCJ1XCI6NDc2NzU4MSxcInJcIjpcIlVTXCIsXCJzXCI6W1wiV1wiLFwiRlwiLFwiSVwiLFwiVVwiLFwiS1wiLFwiQ1wiXSxcInpcIjpbXSxcInRcIjowfSIsImlhdCI6MTUzNzMyMTkyOH0.r8MaouEsyTiWJ0qPqUt2McslSPP2NTinL9YrnQ9Lcow");
-        }
-
         [Test]
         public void ContactsProperty_ShouldReturnContactsClient()
         {
-            Assert.IsInstanceOf(typeof(IWrikeContactsClient), _wrikeClient.Contacts);
+            Assert.IsInstanceOf(typeof(IWrikeContactsClient), TestConstants.WrikeClient.Contacts);
+        }        
+
+        [Test]
+        [TestCaseSource(typeof(TestConstants), "StringListParameterCanNotBeNullOrEmptyAndCanNotHaveMoreThanHundredItems")]
+        public void GetAsyncWithIds_Throws<T>(T argumentException, List<string> ids) where T : ArgumentException
+        {
+            
+            Assert.ThrowsAsync<T>(() => TestConstants.WrikeClient.Contacts.GetAsync(ids));
         }
 
         [Test]
-        public void GetAsync_ContactIdsMoreThanHundred_ThrowArgumentException()
+        [TestCaseSource(typeof(TestConstants), "SrtingParameterCanNotBeNullOrEmpty")]
+        public void UpdateAsync_Throws<T>(T argumentException, string id) where T : ArgumentException
         {
-            var contactIds = new List<string>();
-            for (int i = 0; i < 101; i++)
-            {
-                contactIds.Add($"contactId {i}");
-            }
-
-            Assert.ThrowsAsync<ArgumentException>(() => _wrikeClient.Contacts.GetAsync(contactIds));
+            Assert.ThrowsAsync<T>(() => TestConstants.WrikeClient.Contacts.UpdateAsync(id));
         }
 
-        [Test]
-        public void GetAsync_ContactIdsNull_ThrowArgumentNullException()
-        {
-            List<string> contactIds = null;
-
-            Assert.ThrowsAsync<ArgumentNullException>(() => _wrikeClient.Contacts.GetAsync(contactIds));
-        }
-
-        [Test]
-        public void GetAsync_ContactIdsEmpty_ThrowArgumentException()
-        {
-            var contactIds = new List<string>();
-
-            Assert.ThrowsAsync<ArgumentException>(() => _wrikeClient.Contacts.GetAsync(contactIds));
-        }
-
-        [Test]
-        public void UpdateAsync_IdNull_ThrowArgumentNullException()
-        {
-            string id = null;
-
-            Assert.ThrowsAsync<ArgumentNullException>(() => _wrikeClient.Contacts.UpdateAsync(id));
-        }
-
-        [Test]
-        public void UpdateAsync_IdEmpty_ThrowArgumentException()
-        {
-            string id = string.Empty;
-
-            Assert.ThrowsAsync<ArgumentException>(() => _wrikeClient.Contacts.UpdateAsync(id));
-        }
+        
     }
+
 }
 
 
---
+++ Taviloglu.Wrike.ApiClient.Tests.Unit/TestConstants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Taviloglu.Wrike.ApiClient.Tests.Unit
+{
+    public static class TestConstants
+    {
+        public static ArgumentException ArgumentException = new ArgumentException();
+
+        public static ArgumentNullException ArgumentNullException = new ArgumentNullException();
+
+        public static WrikeClient WrikeClient = new WrikeClient("");
+
+        public static object[] SrtingParameterCanNotBeNullOrEmpty =
+        {
+            new object[] { ArgumentException, string.Empty },
+            new object[] { ArgumentNullException, null }
+        };
+
+        public static object[] StringListParameterCanNotBeNullOrEmptyAndCanNotHaveMoreThanHundredItems =
+        {
+            new object[] { ArgumentException, new List<string>() },
+            new object[] { ArgumentNullException, null },
+            new object[] { ArgumentException, ListOfStringWithMoreThanHundredItems() },
+        };
+
+        public static List<string> ListOfStringWithMoreThanHundredItems()
+        {
+            var retVal = new List<string>();
+
+            for (int i = 0; i < 101; i++) { retVal.Add("id"); }
+
+            return retVal;
+        }
+    }
+}
