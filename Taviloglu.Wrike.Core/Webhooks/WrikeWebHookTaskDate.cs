120682b3c3632bd316c12065866b82250fe4e77d
Sinan Taviloglu <staviloglu@gmail.com>
2018-09-24 01:47:47 +0300

    - Remove&Sort usings

--- Taviloglu.Wrike.Core/Webhooks/WrikeWebHookTaskDate.cs
+++ Taviloglu.Wrike.Core/Webhooks/WrikeWebHookTaskDate.cs
@@ -1,10 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Taviloglu.Wrike.Core.Json;
 
 namespace Taviloglu.Wrike.Core
