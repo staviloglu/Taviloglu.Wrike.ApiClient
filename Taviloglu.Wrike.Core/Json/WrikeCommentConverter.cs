f395f2146a692e51f239984a55a25d99f56b91a8
Sinan Taviloglu <staviloglu@gmail.com>
2018-09-24 01:47:22 +0300

    - WrikeCommentConverter added

---
+++ Taviloglu.Wrike.Core/Json/WrikeCommentConverter.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taviloglu.Wrike.Core.Json
+{
+    class WrikeCommentConverter : JsonConverter
+    {        
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(WrikeComment);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jObject = JObject.Load(reader);
+
+            JToken token;
+
+            bool isFolderComment = jObject.TryGetValue("folderId", out token) && token.Type == JTokenType.String;
+            if (isFolderComment)
+            {
+                return jObject.ToObject<WrikeFolderComment>();
+            }
+            else
+            {
+                return jObject.ToObject<WrikeTaskComment>();
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected bool FieldExists(
+        JObject jObject,
+        string name,
+        JTokenType type)
+        {
+            JToken token;
+            return jObject.TryGetValue(name, out token) && token.Type == type;
+        }
+    }
+}
