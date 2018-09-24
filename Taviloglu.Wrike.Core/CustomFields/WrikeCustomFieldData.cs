825acc0e3ccdad56b5c0227777f22aa652933fb7
Sinan Taviloglu <staviloglu@gmail.com>
2018-09-24 17:56:19 +0300

    - CustomFieldData modified; ObjectWithId inheritance removed Defensive coding for ctor Id property changed to CustomField Id

--- Taviloglu.Wrike.Core/CustomFields/WrikeCustomFieldData.cs
+++ Taviloglu.Wrike.Core/CustomFields/WrikeCustomFieldData.cs
@@ -1,14 +1,47 @@
 ﻿using Newtonsoft.Json;
+using System;
 
 namespace Taviloglu.Wrike.Core
 {
-    public sealed class WrikeCustomFieldData : WrikeObjectWithId
+    public sealed class WrikeCustomFieldData : IWrikeObject
     {
-        public WrikeCustomFieldData() { }
-        public WrikeCustomFieldData(string id, string value) {
-            Id = id;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WrikeCustomFieldData"/> class.
+        /// </summary>
+        /// <param name="customFieldId">Custom Field ID</param>
+        /// <param name="value">Custom Field Value</param>
+        public WrikeCustomFieldData(string customFieldId, string value) {
+
+            if (customFieldId == null)
+            {
+                throw new ArgumentNullException(nameof(customFieldId));
+            }
+
+            if (customFieldId == string.Empty)
+            {
+                throw new ArgumentException($"{nameof(customFieldId)} can not be empty string",nameof(customFieldId));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (value == string.Empty)
+            {
+                throw new ArgumentException($"{nameof(value)} can not be empty string", nameof(value));
+            }
+
+            CustomFieldId = customFieldId;
             Value = value;
         }
+
+        /// <summary>
+        /// Custom field id
+        /// </summary>
+        [JsonProperty("id")]
+        public string CustomFieldId { get; }
+
         /// <summary>
         /// Custom field value
         /// </summary>
