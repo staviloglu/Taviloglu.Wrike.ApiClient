d19258d13d06418ef676b8d8fa52eb2b8f4c8520
Sinan Taviloglu <staviloglu@gmail.com>
2018-09-24 17:57:04 +0300

    - Deleted WrikeException

--- Taviloglu.Wrike.Core/WrikeException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Taviloglu.Wrike.Core
-{
-    public sealed class WrikeException : Exception
-    {
-        public WrikeException(string error, string errorDescription) : base($"{error}: {errorDescription}") { }
-    }
-}
